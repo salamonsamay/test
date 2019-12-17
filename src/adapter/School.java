84da0d0f9f68067eea2c93532249f03485c3a369
salamon <salamon.salamon0@gmail.com>
2019-12-17 13:12:55 +0200

    test2

--- src/adapter/School.java
+++ src/adapter/School.java
@@ -1,14 +1,34 @@
 package adapter;
 
-public class School {
+public class School implements Runnable{
 
 	public static void main(String[] args) {
+		School s=new School();
+		Thread t=new Thread(s);
+		t.start();
+		
+	   for(int i=0;i<100000;i=i+2)
+		   System.out.println(i);
+	}
+
+	@Override
+	public void run() {
 		// TODO Auto-generated method stub
-		System.out.println(234);
-		Pen p=new PenAdapter();
-        AssigmentWork aw=new AssigmentWork();
-        aw.setP(p);
-        aw.writeAssigment("test");
+		printOdd();
+		
+		
+	}
+	
+	public void printOdd(){
+		for(int i=1;i<1000;i=i+2){
+			System.out.println(i);
+			try {
+				Thread.sleep(233);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
